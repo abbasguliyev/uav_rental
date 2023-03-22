98d90996c5eaf17443c18cf4a75db4bf4f611e49
abbasguliyev <abbasquliyev111@gmail.com>
2023-03-23 00:38:58 +0400

    new features

--- src/uav_rental/urls.py
+++ src/uav_rental/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import handler400, handler403, handler404, handler500
 from rest_framework import permissions
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from django.views.static import serve
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -44,9 +45,9 @@ urlpatterns = [
     path('', include('apps.uav.urls')),
     path('account/', include('apps.account.urls')),
     path('order/', include('apps.order.urls')),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 handler404 = 'apps.uav.views.custom_page_not_found_view'
